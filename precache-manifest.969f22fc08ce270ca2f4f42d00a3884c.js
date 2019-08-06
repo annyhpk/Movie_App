7c3439b188e41e26025cbdb78c9285f01a3cdbbb
ChanHo Kim <cksgh063@gmail.com>
2019-08-07 01:01:15 +0900

    Updates

---
+++ precache-manifest.969f22fc08ce270ca2f4f42d00a3884c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689f8eb16808dfc50a37fe129d11ef37",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "90fd5d907d3d6ced7855",
+    "url": "/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/movie_app/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "90fd5d907d3d6ced7855",
+    "url": "/movie_app/static/js/main.77427601.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
