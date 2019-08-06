7c353261a7c155ef90ca1a9a02dc1221816f46be
ChanHo Kim <cksgh063@gmail.com>
2019-08-07 00:55:27 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,68 +1,52 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-    "http://recipe1.ezmember.co.kr/cache/recipe/2015/04/02/238f0a3e8b567db1229b0f27220033341.jpg",
-    rating: 5.0
-
-  },
-  {
-    id: 2,
-    name: "Kimbob",
-    image:
-    "http://recipe1.ezmember.co.kr/cache/recipe/2016/04/08/1d26c0444e724bca8ed271b24da0057a1.jpg",
-    rating: 4.6
-  },
-  {
-    id: 3,
-    name: "Ramen",
-    image:
-    "https://0.soompi.io/wp-content/uploads/2013/05/ramen-main.jpg?s=900x600&e=t",
-    rating: 4.9
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-    "http://img.sbs.co.kr/newsnet/etv/upload/2014/01/10/30000347969_700.jpg",
-    rating: 4.3
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() => {
+    const { 
+      data: { 
+        data: { movies } 
+      } 
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
   }
-];
-
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I Like {name}</h2>
-      <h4>rating is {rating}/5.0</h4>
-      <img src={picture} alt={name} height="300px" />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: propTypes.string.isRequired,
-  picture: propTypes.string.isRequired,
-  rating: propTypes.number
-};
-
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello React</h1>
-        {foodILike.map((dish) => (
-          <Food 
-          key={dish.id} 
-          name={dish.name} 
-          picture={dish.image} 
-          rating={dish.rating}/>
-        ))}
-    </div>
-  );
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : ( 
+          <div className="movies">
+            {movies.map(movie => {
+          return <Movie 
+                key={movie.id} 
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image}
+                genres={movie.genres} 
+              />
+            })}
+          </div>
+        )}
+      </section>
+      );
+  }
 }
 
 export default App;
